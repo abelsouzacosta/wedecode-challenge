44557ec72d1c9d3167108fdcc05cfbea1e728cd5
Abel Souza <abelsouzacosta@gmail.com>
2021-07-26 20:48:22 -0300

    feat(errors): middleware de tratamento de erros
    Como dentro da aplicação podem ocorrer erros de assíncronos esse middleware vai servir como a camada
    superior que lida com o lançamento desses erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,11 +3,32 @@ import 'express-async-errors';
 import 'dotenv/config';
 import cors from 'cors';
 import '@shared/typeorm';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import ApplicationError from '@shared/errors/ApplicationError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// middleware usado par o tratametno de erros assíncronos
+app.use(
+  // eslint-disable-next-line
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ApplicationError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+        stack: error.stack,
+      });
+    }
+
+    return response.status(400).json({
+      status: 'error',
+      message: error.message,
+      stack: error.stack,
+    });
+  },
+);
 
 app.listen(process.env.PORT);
