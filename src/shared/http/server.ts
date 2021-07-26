cd734619a6a8e2a2cd753fd69e2a504813abc70b
Abel Souza <abelsouzacosta@gmail.com>
2021-07-26 20:53:42 -0300

    feat(routes): adicionando módulo que lida com as rotas da aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,11 +5,14 @@ import cors from 'cors';
 import '@shared/typeorm';
 import express, { NextFunction, Request, Response } from 'express';
 import ApplicationError from '@shared/errors/ApplicationError';
+import { router } from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
 
 // middleware usado par o tratametno de erros assíncronos
 app.use(
