4492cf88a58b123c930eb6fab8c8da4250403e7e
Abel Souza <abelsouzacosta@gmail.com>
2021-07-26 23:43:13 -0300

    feat(spectators): list
    Listagem de espectadores

--- src/modules/spectators/controllers/SpectatorsController.ts
+++ src/modules/spectators/controllers/SpectatorsController.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
 import CreateSpectatorService from '../services/CreateSpectatorService';
+import ListSpectatorsService from '../services/ListSpectatorsService';
 
 export default class SpectatorsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const list = new ListSpectatorsService();
+
+    const spectators = await list.execute();
+
+    return response.status(200).json(spectators);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { name } = request.body;
 
--- src/modules/spectators/routes/index.ts
+++ src/modules/spectators/routes/index.ts
@@ -4,6 +4,8 @@ import SpectatorsController from '../controllers/SpectatorsController';
 const spectatorsRouter = Router();
 const controller = new SpectatorsController();
 
+spectatorsRouter.get('/', controller.index);
+
 spectatorsRouter.post('/', controller.create);
 
 export { spectatorsRouter };
