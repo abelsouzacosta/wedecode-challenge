1213312776a9bf71df357bc27bbdb5de96d8b853
Abel Souza <abelsouzacosta@gmail.com>
2021-07-27 02:20:23 -0300

    chore(spectator_movie): repositorio
    Adicionando ufnção responsável por buscar todas as relações de um determinado espectador dentro do
    banco de dados

--- src/modules/spectators/typeorm/repositories/SpectatorMovieRepository.ts
+++ src/modules/spectators/typeorm/repositories/SpectatorMovieRepository.ts
@@ -3,10 +3,11 @@ import { SpectatorMovie } from '../entities/SpectatorMovie';
 
 @EntityRepository(SpectatorMovie)
 export class SpectatorMovieRepository extends Repository<SpectatorMovie> {
+  // ao receber os parâmetros vai inserir as informações dentro do banco de dados
   public async createWatch(
     spectator_id: string,
     movie_id: string,
-  ): Promise<SpectatorMovie> {
+  ): Promise<SpectatorMovie | undefined> {
     const watch = this.create({
       movie_id,
       spectator_id,
@@ -15,5 +16,18 @@ export class SpectatorMovieRepository extends Repository<SpectatorMovie> {
     await this.save(watch);
 
     return watch;
+  }
+
+  // faz uma busca pelo id do espectador
+  public async findBySpectatorId(
+    spectator_id: string,
+  ): Promise<SpectatorMovie[] | undefined> {
+    const spectator = await this.find({
+      where: {
+        spectator_id,
+      },
+    });
+
+    return spectator;
   }
 }
